118b12a7e80a836dab65b1e8760a614b07ad55ab
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-03-28 18:54:44 +0200

    character 3D models addition

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    Transform transform;
+    void Start()
+    {
+        transform = gameObject.GetComponent<Transform>();
+    }
+    void Update()
+    {
+        transform.position += new Vector3(0.01f,0,0);
+    }
+}
