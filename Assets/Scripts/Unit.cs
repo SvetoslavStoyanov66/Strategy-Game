86f0b60d4295ab46dca27b38eb29845142b3e49d
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-03-30 18:37:13 +0200

    basic movement

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,15 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Data.Common;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Unit : MonoBehaviour
 {
+    Camera myCam;
+    NavMeshAgent myAgent;
+    public LayerMask ground;
     void Start()
     {
         UnitSelect.Instance.allUnits.Add(this.gameObject);
+        myAgent = GetComponent<NavMeshAgent>();
+        myCam = Camera.main;
     }
     void Destroy()
     {
         UnitSelect.Instance.allUnits.Remove(this.gameObject);
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1) && UnitSelect.Instance.selectedUnits.Contains(this.gameObject))
+        {
+            RaycastHit hit;
+            Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            {
+                myAgent.SetDestination(hit.point);
+            }
+
+        }
+        
     }
 }
