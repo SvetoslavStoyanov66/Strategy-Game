2566dad8125b58871d71241126cdbbd2153431ac
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-03-30 21:16:45 +0200

    Animations and selection improvements

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,11 +7,14 @@ public class Unit : MonoBehaviour
     Camera myCam;
     NavMeshAgent myAgent;
     public LayerMask ground;
+    Animator animator;
+    bool isMoving = false;
     void Start()
     {
         UnitSelect.Instance.allUnits.Add(this.gameObject);
         myAgent = GetComponent<NavMeshAgent>();
         myCam = Camera.main;
+        animator = GetComponent<Animator>();
     }
     void Destroy()
     {
@@ -20,17 +23,25 @@ public class Unit : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(1) && UnitSelect.Instance.selectedUnits.Contains(this.gameObject))
+        if (Input.GetMouseButtonDown(1) && UnitSelect.Instance.selectedUnits.Contains(this.gameObject))
         {
             RaycastHit hit;
             Ray ray = myCam.ScreenPointToRay(Input.mousePosition);
 
-            if(Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ground))
             {
                 myAgent.SetDestination(hit.point);
+                isMoving = true; // Set the flag to indicate movement
             }
-
         }
-        
+
+        // Check if the agent is moving and update the animator accordingly
+        animator.SetBool("isRunning", isMoving);
+
+        // Check if the agent has reached its destination
+        if (isMoving && !myAgent.pathPending && myAgent.remainingDistance <= myAgent.stoppingDistance + 0.8f)
+        {
+            isMoving = false; // Set the flag to indicate that the unit has stopped moving
+        }
     }
 }
